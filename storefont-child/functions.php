26cc73fa1ea5760722cfc3cc4f03970fc52e7ea3
gcolind <gcolin@pdx.edu>
2015-05-24 18:22:53 -0700

    css fixes/fonts added

--- storefont-child/functions.php
+++ storefont-child/functions.php
@@ -6,4 +6,13 @@ function theme_enqueue_styles() {
 
 }
 
+// Register and enqueue styles
+function storefrontchild_load_style() {
+	if ( !is_admin() ) {
+	    wp_enqueue_style( 'storefront_damion', 'http://fonts.googleapis.com/css?family=Damion');
+	    wp_enqueue_style( 'storefront_style', get_stylesheet_uri() );
+	}
+}
+add_action('wp_enqueue_scripts', 'storefrontchild_load_style');
+
 ?>
